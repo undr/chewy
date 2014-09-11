a64c8c9c6121f9475879f24214f99eaed4fcc08c
undr <undr@yandex.ru>
2014-09-11 19:04:10 +0700

    Fixed index creation without ActiveRecord existence

--- lib/chewy/type.rb
+++ lib/chewy/type.rb
@@ -5,7 +5,7 @@ module Chewy
     def self.new(index, target, options = {}, &block)
       type = Class.new(Chewy::Type::Base)
 
-      adapter = if (target.is_a?(Class) && target < ActiveRecord::Base) || target.is_a?(::ActiveRecord::Relation)
+      adapter = if is_target_activerecord?(target)
         Chewy::Type::Adapter::ActiveRecord.new(target, options)
       else
         Chewy::Type::Adapter::Object.new(target, options)
@@ -17,6 +17,13 @@ module Chewy
 
       type.class_eval &block if block
       type
+    end
+
+    private
+
+    def self.is_target_activerecord?(target)
+      return false unless defined?(::ActiveRecord)
+      (target.is_a?(Class) && target < ActiveRecord::Base) || target.is_a?(::ActiveRecord::Relation)
     end
   end
 end
